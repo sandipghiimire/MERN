6d0d678e59121ade22f8b73686f196fcf43a1fa5
Sandip <sandipghimire254@gmail.com>
2023-12-02 19:10:39 +0545

    Thsi is file added

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("Hello World");
+// What is Java Script ?
+// JavaScript is a High level, objec based, multi paradigm, interpretrd programming language.
+
+//compiler - source code - compile - machine code - output (c, java)
+//interprated - source code - interprate - output (javascript)
---
+++ veriables.js
@@ -0,0 +1,44 @@
+//veriables - They are veru important concep in every programming language that are used to hold (store) data and manipulate them.
+//veriables can store valuas and expressions.
+//Expressions are something that will evantually resolves to a value.
+
+//Javascript is dynamically Typed programming language.
+
+//Syntax:
+//variable_type variable_name (identifier) = (assignment operator) value or expression.
+
+//There are three ways to declare a variables in javascript. They are:-
+//1. Var (Pre-ES6)
+//2. let (ES6+)
+//3. const (ES6+)
+
+//Rules for Identifier:
+//1. Identifier should always start with (a-z, A-Z, _, $)
+//2. It shouldn't start with number or special characters except($,_).
+//3. They are case sensative. It mean x and X are two different.
+//4. There shouldn't be space between the identifier.
+
+//Standard Conventions for Identifier in JS.
+//1. Variables names should be in camelCase (eg, var myName;)
+//2. Identifier must be descriptive and relevent to what kind of values it must hold(store).
+
+//Pascel Case (Every Word First Letter Capital) - oop
+
+//JavaScript is backward compatible pragramming language.
+
+//var vs let vs const
+//var - var is used to declare variable which value can or any change overtime in our program.
+//2. Variable decaration only possible in var.(eg. var firstName;)
+var firstName; // value Decelaration 
+firstName="Sandip"; // value assign  
+//3. Variable re-decleration is possible in var.
+
+//let - let is used to declare variable which value can or any change overtime in our program.
+//2. Variable decaration only possible in let. (eg. let lastName;)
+let lastName; // value decelaration 
+lastName="Ghimire"; //value assign
+//3. Variable re-decleration is not possible in let.
+
+//const - it is used to declare constant veriable.
+//2. Variable decaration only is not possible in Constant.(eg. const address;x)
+//3. Variable re-decleration is not possible in const.
