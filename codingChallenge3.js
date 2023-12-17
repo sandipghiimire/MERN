1613daf0f66e403043e35ee32e6ba8610c59815a
Sandip <sandipghimire254@gmail.com>
2023-12-17 22:12:37 +0545

    Assignment 3 is updated!

--- codingChallenge3.js
+++ codingChallenge3.js
@@ -1,38 +1,22 @@
-// Test data
-// Data 1
+
 const dolphinsScores1 = [96, 108, 89];
 const koalasScores1 = [88, 91, 110];
 
-// Data Bonus 1
+
 const dolphinsScores2 = [97, 112, 101];
 const koalasScores2 = [109, 95, 123];
 
-// Data Bonus 2
+
 const dolphinsScores3 = [97, 112, 101];
 const koalasScores3 = [109, 95, 106];
 
-// Function to calculate average score
-const calculateAverage = (scores) => scores.reduce((sum, score) => sum + score, 0) / scores.length;
-
-// Function to determine the winner
-const determineWinner = (dolphins, koalas) => {
-  const avgDolphins = calculateAverage(dolphins);
-  const avgKoalas = calculateAverage(koalas);
-
-  // Bonus 1: Check for minimum score requirement
-  if (avgDolphins >= 100 && avgKoalas >= 100) {
-    // Bonus 2: Check for draw with minimum score requirement
-    if (avgDolphins === avgKoalas) {
-      return "It's a draw!";
-    }
-
-    return avgDolphins > avgKoalas ? "Dolphins win!" : "Koalas win!";
+const avgDolphins = (dolphinsScores1[0]+dolphinsScores1[1]+dolphinsScores1[2]+dolphinsScores2[0]+dolphinsScores2[1]+dolphinsScores2[2]+ dolphinsScores3[0]+dolphinsScores3[1]+dolphinsScores3[2])/9;
+const avgKoalas = (koalasScores1[0]+koalasScores1[1]+koalasScores1[2]+koalasScores2[0]+koalasScores2[1]+koalasScores2[2]+koalasScores3[0]+koalasScores3[1]+koalasScores3[2])/9;
+  
+  if (avgDolphins>avgKoalas){
+    console.log("Winner is dolphins!")
+  } else if (avgKoalas>avgDolphins) {
+    console.log("The winner is Koalas!")
   } else {
-    return "No team wins the trophy!";
-  }
-};
-
-// Test the data
-console.log("Data 1:", determineWinner(dolphinsScores1, koalasScores1));
-console.log("Data Bonus 1:", determineWinner(dolphinsScores2, koalasScores2));
-console.log("Data Bonus 2:", determineWinner(dolphinsScores3, koalasScores3));
+    console.log('Its a draw')
+  };
