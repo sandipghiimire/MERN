827c1ac4cef1b97cd05796b752d1abc714cc456e
Sandip <sandipghimire254@gmail.com>
2023-12-17 21:44:50 +0545

    Update code for Assignment 2

--- codingChallenge2.js
+++ codingChallenge2.js
@@ -1,4 +1,5 @@
 
+
 let markMass = 78;
 let markHeight = 1.69;
 
@@ -6,12 +7,11 @@ let johnMass = 92;
 let johnHeight = 1.95;
 
 
-let markBMI = markMass / (markHeight ** 2);
-let johnBMI = johnMass / (johnHeight ** 2);
+let markBMI = markMass / (markHeight * markHeight);
+let johnBMI = johnMass / (johnHeight * johnHeight);
 
-
-let output = markBMI > johnBMI
-  ? `Mark's BMI (${markBMI.toFixed(1)}) is higher than John's (${johnBMI.toFixed(1)})!`
-  : `John's BMI (${johnBMI.toFixed(1)}) is higher than Mark's (${markBMI.toFixed(1)})!`;
-
-console.log(output);
+if (markBMI>johnBMI){
+  console.log("Mark's BMI is higher than John's!")
+} else {
+  console.log("John's BMI is higher than Mark's!")
+};
