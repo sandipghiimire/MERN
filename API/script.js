7f10b2ac7ec75eac37f5a3498c450a5672a2a23b
Sandip Ghimire <sandipghimire254@gmail.com>
2024-07-08 16:54:10 +0545

    Update

--- API/script.js
+++ API/script.js
@@ -1,30 +1,35 @@
-document.getElementById('searchBtn').addEventListener('click', searchImages);
+// document.getElementById('searchBtn').addEventListener('click', searchImages);
 
-function searchImages() {
-    const searchTerm = document.getElementById('searchInput').value;
-    const apiKey = 'YOUR_RAPIDAPI_KEY'; // Replace with your RapidAPI key
-    const apiHost = 'real-time-image-search.p.rapidapi.com';
-    const apiUrl = `https://${apiHost}/search?q=${searchTerm}`;
+// function searchImages() {
+//     const searchTerm = document.getElementById('searchInput').value;
+//     const apiKey = 'YOUR_RAPIDAPI_KEY'; // Replace with your RapidAPI key
+//     const apiHost = 'real-time-image-search.p.rapidapi.com';
+//     const apiUrl = `https://${apiHost}/search?q=${searchTerm}`;
 
-    fetch(apiUrl, {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': apiKey,
-            'X-RapidAPI-Host': apiHost
-        }
-    })
-    .then(response => response.json())
-    .then(data => {
-        const resultsDiv = document.getElementById('results');
-        resultsDiv.innerHTML = '';
-        data.value.forEach(image => {
-            const imgElement = document.createElement('img');
-            imgElement.src = image.url;
-            resultsDiv.appendChild(imgElement);
-        });
-    })
-    .catch(error => {
-        console.error('Error fetching the image data:', error);
-        document.getElementById('results').innerHTML = `Error fetching the image data.`;
-    });
-}
+//     fetch(apiUrl, {
+//         method: 'GET',
+//         headers: {
+//             'X-RapidAPI-Key': apiKey,
+//             'X-RapidAPI-Host': apiHost
+//         }
+//     })
+//     .then(response => response.json())
+//     .then(data => {
+//         const resultsDiv = document.getElementById('results');
+//         resultsDiv.innerHTML = '';
+//         data.value.forEach(image => {
+//             const imgElement = document.createElement('img');
+//             imgElement.src = image.url;
+//             resultsDiv.appendChild(imgElement);
+//         });
+//     })
+//     .catch(error => {
+//         console.error('Error fetching the image data:', error);
+//         document.getElementById('results').innerHTML = `Error fetching the image data.`;
+//     });
+// }
+
+const accessKey = "mGq-ilFFXqWUoXfTTv3PwNgUkVLMp8iBniTbRmmBzr4";
+const formEl = document.querySelector("form");
+const inputE = document.getElementById("search-input");
+const searchResults = document.querySelector("search-result")
